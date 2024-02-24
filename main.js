dfeb2a724415ed726fccfb92c17ad5ab60b50253
GiovanniPeccati <giovannispano008@gmail.com>
2024-02-24 18:02:42 +0100

    Second version of the program

--- main.js
+++ main.js
@@ -19,8 +19,14 @@ const daysOfWeek = [
 const dayOfWeek = daysOfWeek[now.getDay()];
 
 // parts for possible random messages combinations
-const predictions = ['good luck', 'bad luck', 'a wonderful day', 'a stressful day', 'a fun day', 'an enjoyable day', 'a special day', 'a boring day', 'a tiring day', 'an odd day', 'a crazy day', 'a successful day', 'a very long day'];
-const suggestions = ['stay at home', 'go for a walk', 'hang out with friends', 'do some chores', 'work', 'spend time with your family', 'read a book', 'clean your room', 'do the laundry', 'go to work', 'go to sleep early', 'go to the gym', 'do the dusting', 'take a shower'];
+const goodPredictions = ['good luck', 'a wonderful day', 'a fun day', 'an enjoyable day', 'a special day',  'a successful day', 'a crazy day', 'a relaxing day', 'a good day']
+const goodSuggestions = ['go for a walk', 'hang out with friends', 'spend time with your family', 'go to the gym', 'meet new people', 'try something new', 'go on a roadtrip']
+
+const badPredictions = ['bad luck', 'a stressful day', 'a boring day', 'a tiring day', 'a very long day', 'a tough day', 'an outrageous day', 'a strange day', 'a bad day']
+const badSuggestions = ['stay at home', 'read a book', 'go to sleep early', 'take a shower', 'relax a bit', 'take a nap', 'drink some water', 'eat something']
+
+const allPredictions = ['good luck', 'a wonderful day', 'a fun day', 'an enjoyable day', 'a special day',  'a successful day', 'a crazy day', 'a relaxing day', 'a good day', 'bad luck', 'a stressful day', 'a boring day', 'a tiring day', 'a very long day', 'a tough day', 'an outrageous day', 'a strange day', 'a bad day'];
+const allSuggestions = ['go for a walk', 'hang out with friends', 'spend time with your family', 'go to the gym', 'meet new people', 'try something new', 'go on a roadtrip','stay at home', 'read a book', 'go to sleep early', 'take a shower', 'relax a bit', 'take a nap', 'drink some water', 'eat something'];
 
 // select the message part randomly
 const randomMessage = (arr) => {
@@ -28,8 +34,21 @@ const randomMessage = (arr) => {
 }
 
 function futureReveal() {
-  const prediction = randomMessage(predictions);
-  const suggestion = randomMessage(suggestions);
+  let prediction = randomMessage(allPredictions);;
+  let suggestion;
+
+  // selects the suggestion based on the type of prediction
+  for (let i = 0; i < goodPredictions.length; i++) {
+    if (prediction === goodPredictions[i]) {
+      suggestion = randomMessage(goodSuggestions);
+    } 
+  }
+
+  for (let i = 0; i < badPredictions.length; i++) {
+    if (prediction === badPredictions[i]) {
+      suggestion = randomMessage(badSuggestions);
+    } 
+  }
 
   reveal.style.display = 'none';
 
